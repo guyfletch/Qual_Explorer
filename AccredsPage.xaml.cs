6e9b624f6c1ecf16297c90719896c5e085017e71
guyfletch <guyfletch@gmail.com>
2018-10-17 23:48:17 +0100

    Image button

---
+++ AccredsPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Qual_Explorer
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AccredsPage : ContentPage
+    {
+        public AccredsPage(List<Method> accreds)
+        {
+            InitializeComponent();
+            BindingContext = accreds;
+        }
+
+        async void AccredsList_OnItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (e == null) return;
+            await Navigation.PushAsync(new AccredItem((Method)e.Item));
+            ((ListView)sender).SelectedItem = null;
+        }
+    }
+}
---
+++ App.cs
@@ -0,0 +1,27 @@
+﻿using Xamarin.Forms;
+
+namespace Qual_Explorer
+{
+    public class App : Application
+    {
+        public App()
+        {
+            MainPage = new NavigationPage(new QualPage(DataStore.Instance));
+        }
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
+
